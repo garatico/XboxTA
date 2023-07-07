6083b91b2d53b5d2f6140c33a1afbe95dd5d61d4
Giovanni Aratico <garatico@asu.edu>
2023-07-06 18:23:03 -0700

    Attempting interactive plot

--- src/r/plots/plots_lb.R
+++ src/r/plots/plots_lb.R
@@ -16,4 +16,89 @@ plot_lb_range = function(df, column, start, end, bin_width, scalexby) {
 }
 
 
+plot_lb_range_interactive <- function(df, column, start, end, bin_width, scalexby) {
+  # Create the HTML code
+  html <- sprintf('
+    <html>
+    <head>
+      <title>Interactive Histogram</title>
+      <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
+    </head>
+    <body>
+      <div id="plot"></div>
+      <div>
+        <label for="start">Start:</label>
+        <input type="number" id="start" value="%d" step="1">
+      </div>
+      <div>
+        <label for="end">End:</label>
+        <input type="number" id="end" value="%d" step="1">
+      </div>
+      <div>
+        <label for="bin_width">Bin Width:</label>
+        <input type="number" id="bin_width" value="%d" step="1">
+      </div>
+      <div>
+        <label for="scalexby">Scale X by:</label>
+        <input type="number" id="scalexby" value="%d" step="1">
+      </div>
+      <script>
+        var data = %s;
+        var plotDiv = document.getElementById("plot");
+        var startInput = document.getElementById("start");
+        var endInput = document.getElementById("end");
+        var binWidthInput = document.getElementById("bin_width");
+        var scalexbyInput = document.getElementById("scalexby");
+        
+        function updatePlot() {
+          var start = parseInt(startInput.value);
+          var end = parseInt(endInput.value);
+          var binWidth = parseInt(binWidthInput.value);
+          var scalexby = parseInt(scalexbyInput.value);
+          
+          var filteredData = data.filter(function(d) {
+            return d >= start && d <= end;
+          });
+          
+          var trace = {
+            x: filteredData,
+            type: "histogram",
+            marker: { color: "blue" }
+          };
+          
+          var layout = {
+            title: "Distribution of %s (" + start + " to " + end + ")",
+            xaxis: { title: "%s", range: [start, end], dtick: scalexby },
+            yaxis: { title: "Frequency" },
+            bargap: 0.1,
+            bargroupgap: 0.2
+          };
+          
+          Plotly.newPlot(plotDiv, [trace], layout);
+        }
+        
+        updatePlot();
+        
+        startInput.addEventListener("input", updatePlot);
+        endInput.addEventListener("input", updatePlot);
+        binWidthInput.addEventListener("input", updatePlot);
+        scalexbyInput.addEventListener("input", updatePlot);
+      </script>
+    </body>
+    </html>
+  ', start, end, bin_width, scalexby, jsonlite::toJSON(df[[column]]), column, column)
+  
+  # Save the HTML code to a file
+  writeLines(html, con = "lb_plot.html")
+  # Display the plot within the RMD cell
+  includeHTML("lb_plot.html")
+}
+
+
+
+
+
+
+
+
 
