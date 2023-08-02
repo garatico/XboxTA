eb4378455b93ac324ccea0b8f762a0b4a6cce4ca
Giovanni Aratico <garatico@asu.edu>
2023-08-02 03:18:21 -0700

    Anonymize methods + data added

---
+++ src/py/anonymize.py
@@ -0,0 +1,75 @@
+import os
+import shutil
+import random
+import string
+import pandas as pd
+import re
+
+def generate_random_id(length):
+    characters = string.ascii_uppercase + string.digits
+    return ''.join(random.choice(characters) for _ in range(length))
+
+def anonymize_filenames_in_directory(nonanon_directory, anon_directory, data_types):
+    id_mapping = {}  # Store generated random IDs for each prefix
+    
+    for data_type in data_types:
+        data_type_directory = os.path.join(nonanon_directory, data_type)
+        file_list = os.listdir(data_type_directory)
+        print(f"Anonymizing {data_type} files...")
+        
+        if not file_list:
+            print(f"No files found in {data_type_directory}.")
+        else:
+            for file in file_list:
+                if f'_{data_type}.csv' in file:
+                    prefix, suffix = file.split('_', 1)  # Split at the first '_'
+                    
+                    if prefix not in id_mapping:
+                        random_id = generate_random_id(6)
+                        id_mapping[prefix] = random_id
+                    else:
+                        random_id = id_mapping[prefix]
+                    
+                    new_filename = f"{random_id}_{data_type}.csv"
+                    source_path = os.path.join(data_type_directory, file)
+                    target_path = os.path.join(anon_directory, data_type, new_filename)
+                    shutil.copy(source_path, target_path)
+                    print(f"Copied {file} to {new_filename}")
+                else:
+                    print(f"File not anonymized: {file}")
+    
+    return id_mapping
+
+# Subsequent method for further processing the anonymized files
+def process_anonymized_filenames(anon_directory, data_types, id_mapping):
+    # Example implementation - Print the mapping of original prefixes to random IDs
+    for data_type in data_types:
+        print(f"Mapping for {data_type}:")
+        for prefix, random_id in id_mapping.items():
+            print(f"{prefix} -> {random_id}")
+
+def remove_gamerid_and_hash_from_urls(df):
+    if "achievement_game_url" in df.columns:
+        df["achievement_game_url"] = df["achievement_game_url"].apply(lambda url: re.sub(r'\?gamerid=\d+', '', url))
+    if "achievement_url" in df.columns:
+        df["achievement_url"] = df["achievement_url"].apply(lambda url: url.split('#', 1)[0])
+    if "game_url" in df.columns:
+        df["game_url"] = df["game_url"].apply(lambda url: re.sub(r'\?gamerid=\d+', '', url))
+    return df
+
+def process_files_in_directory(directory_path):
+    file_list = os.listdir(directory_path)
+    print("Processing files in directory:")
+    
+    if not file_list:
+        print("No files found in the directory.")
+    else:
+        for file in file_list:
+            file_path = os.path.join(directory_path, file)
+            if file.endswith(".csv"):
+                df = pd.read_csv(file_path)
+                df = remove_gamerid_and_hash_from_urls(df)
+                df.to_csv(file_path, index=False)
+                print(f"Processed {file}")
+            else:
+                print(f"File not processed. Only CSV files are supported: {file}")
--- src/r/models/ts_decomp.R
+++ src/r/models/ts_decomp.R
@@ -1,28 +1,34 @@
-da_profiles_ts_decomp = function(da_profiles) {
-  ts_profiles = lapply(seq_along(da_profiles), function(i) {
+da_profiles_ts_decomp <- function(da_profiles) {
+  ts_profiles <- lapply(seq_along(da_profiles), function(i) {
+    # Get the current profile from the list of da_profiles
     profile <- da_profiles[[i]]
     profile <- profile %>% arrange(date)
     
+    # Create a list of time series objects for each variable
     ts_objs <- list(
-      ts(profile$daily_lt_eir, frequency = 366, start = c(profile$year[1], profile$day_of_year[1])),
-      ts(profile$weekly_eir, frequency = 366, start = c(profile$year[1], profile$week[1])),
-      ts(profile$monthly_eir, frequency = 366, start = c(profile$year[1], profile$month.x[1])),
-      ts(profile$days_since_achievement, frequency = 366, start = c(profile$year[1], profile$day_of_year[1])),
-      ts(profile$churn_binary, frequency = 366, start = c(profile$year[1], profile$day_of_year[1]))
+      daily_lt_eir = ts(profile$daily_lt_eir, frequency = 366, start = c(profile$year[1], profile$day_of_year[1])),
+      weekly_eir = ts(profile$weekly_eir, frequency = 366, start = c(profile$year[1], profile$week[1])),
+      monthly_eir = ts(profile$monthly_eir, frequency = 366, start = c(profile$year[1], profile$month.x[1])),
+      days_since_achievement = ts(profile$days_since_achievement, frequency = 366, start = c(profile$year[1], profile$day_of_year[1])),
+      churn_binary = ts(profile$churn_binary, frequency = 366, start = c(profile$year[1], profile$day_of_year[1]))
     )
     
+    # Decompose each time series object and store the decomposition results
     decompositions <- lapply(ts_objs, function(ts_obj) {
       if (length(ts_obj) >= 2 * 366) {
-        decompose(ts_obj)
+        decompose_result <- decompose(ts_obj)
+        return(decompose_result)
       } else {
-        NULL
+        return(NULL)
       }
     })
     
+    # Check if all decompositions are NULL (insufficient data for decomposition)
     if (all(sapply(decompositions, is.null))) {
       print(paste("Insufficient data for profile", i, "- skipping decomposition."))
       return(NULL)
     } else {
+      # Return a list containing profile data, time series objects, and decomposition results
       return(list(
         profile = profile,
         ts = ts_objs,
